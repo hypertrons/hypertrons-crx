63365257125fae1604f64ddcf4404e7df8461d00
LiuChangFreeman <liuchangfreeman@live.com>
2021-03-15 23:03:34 +0800

    compact source code files, all convert to typescript

---
+++ src/pages/Popup/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './index.css';
+
+render(<div />, window.document.querySelector('#app-container'));
--- webpack.config.js
+++ webpack.config.js
@@ -34,9 +34,9 @@ if (fileSystem.existsSync(secretsPath)) {
 var options = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
-    options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
-    popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
-    background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
+    options: path.join(__dirname, 'src', 'pages', 'Options', 'index.tsx'),
+    popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.tsx'),
+    background: path.join(__dirname, 'src', 'pages', 'Background', 'index.ts'),
     contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.ts'),
   },
   output: {
