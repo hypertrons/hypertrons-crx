4fda7ea6900ced43bd3f2b57be2e109b518472d5
LiuChangFreeman <liuchangfreeman@live.com>
2021-03-15 23:38:49 +0800

    fix bug for background tasks not active

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,13 +1,12 @@
-// @ts-ignore
 export enum BackgroundTasks {
   update = 'check_for_updates'
 }
 
-chrome.alarms.create(BackgroundTasks.update,{periodInMinutes:60*24});
+chrome.alarms.create(BackgroundTasks.update,{periodInMinutes:10});
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   const name=alarm.name;
   if(name===BackgroundTasks.update){
-    console.log("check for updates")
+    console.log("check for updates");
   }
 });
