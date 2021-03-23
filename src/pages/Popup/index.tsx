b114d1873a1c33bed4b2497ad7441b32010ae661
LiuChangFreeman <liuchangfreeman@live.com>
2021-03-23 22:42:09 +0800

    add popup page

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import PopupPage from '../../components/PopupPage/index'
 
 import './index.css';
 
-render(<div />, window.document.querySelector('#app-container'));
+render(<PopupPage />, window.document.querySelector('#app-container'));
