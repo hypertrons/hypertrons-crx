f2e02ccc6aa0251969e179d081f8c38d57c33c23
LiuChangFreeman <liuchangfreeman@live.com>
2021-04-03 20:45:50 +0800

    new update notice

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,42 +1,42 @@
-import { compareVersion, getBrowserType} from '../../utils/utils';
-import { getUpdateInfor } from "../../services/background"
+import { chromeSet, compareVersion, getMessageI18n } from '../../utils/utils';
+import { createNotification } from "../../services/background"
+import { checkUpdate } from "../../services/common"
 import { loadSettings } from '../../utils/settings';
+import { loadMetaData } from '../../utils/metadata';
 
 export enum BackgroundTasks {
   update = 'check_for_updates'
 }
 
+// in develop-version, we can do this every 0.1 min, but not in store-version
 chrome.alarms.create(BackgroundTasks.update,{periodInMinutes:0.1});
 
 chrome.alarms.onAlarm.addListener(async (alarm) => {
   const name=alarm.name;
 
   const settings=await loadSettings();
+  const metaData=await loadMetaData();
 
-  if(name===BackgroundTasks.update ){
+
+  if(name===BackgroundTasks.update){
     if(settings.checkForUpdates){
-      // @ts-ignore :we must ignore here
-      const details=chrome.app.getDetails();
-      const currentVersion=details["version"];
-      const browserType=getBrowserType();
-      const updateInformation=await getUpdateInfor();
-      let latestVersion;
-
-      if("key" in details){
-        // the store-version installation
-        if(browserType==="Edge"){
-          latestVersion=updateInformation["edge"]["latest_version"];
-        }
-        else{
-          latestVersion=updateInformation["chrome"]["latest_version"];
-        }
-      }
-      else{
-        latestVersion=updateInformation["develop"]["latest_version"];
-      }
-
+      console.log("check for updates");
+      const [currentVersion,latestVersion]=await checkUpdate();
       if(compareVersion(currentVersion,latestVersion)===-1){
-        console.log(`new ${browserType} update available, version:${latestVersion}`);
+        const timeNow=new Date().valueOf();
+        const duration=timeNow-metaData.timeLastNoticeNewUpdate;
+        console.log(duration);
+        if(duration>=24*60*60*1000){
+          // only notice people once within 24 hours
+          createNotification('check_for_updates', {
+            type: 'basic',
+            iconUrl: 'main.png',
+            title: getMessageI18n('notification_title_newUpdate'),
+            message: getMessageI18n('notification_message_newUpdate').replace('%v', latestVersion),
+          })
+          metaData.timeLastNoticeNewUpdate=timeNow;
+          await chromeSet("meta_data", metaData.toJson());
+        }
       }
     }
   }
