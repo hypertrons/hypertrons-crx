ee985c78626f1ce42596aa0910147428fdc5f40f
LiuChangFreeman <liuchangfreeman@live.com>
2021-04-03 22:15:00 +0800

    feat: implement checking for updates (#82)
    * introduce update background task
    
    * change icon's name
    
    * inline loadSettings in settings.ts
    
    * introduce mocked check for updates

--- src/components/OptionsPage/index.tsx
+++ src/components/OptionsPage/index.tsx
@@ -4,7 +4,7 @@ import {
 } from 'office-ui-fabric-react';
 import { initializeIcons } from '@uifabric/icons';
 import { getMessageI18n, chromeGet, chromeSet, isNull } from '../../utils/utils'
-import Settings from "../../utils/settings"
+import Settings,{ loadSettings } from "../../utils/settings"
 import './index.css';
 
 initializeIcons();
@@ -13,21 +13,25 @@ const OptionsPage: React.FC = () => {
 
   const [settings, setSettings] = useState(new Settings());
   const [inited, setInited] = useState(false);
+  const [version, setVersion] = useState("0.0.0");
 
   useEffect(() => {
     const initSettings = async () => {
-      let obj = await chromeGet("settings");
-      if (isNull(obj)) {
-        obj = {};
-      }
-      settings.loadFromJson(obj);
-      setSettings(settings);
+      const temp=await loadSettings();
+      setSettings(temp);
       setInited(true);
     }
     initSettings();
   }, [settings]);
 
-  const saveSettings = async () => {
+  useEffect(() => {
+    // @ts-ignore
+    const details=chrome.app.getDetails();
+    setVersion(details["version"]);
+
+  }, [version]);
+
+  const saveSettings = async (settings:Settings) => {
     setSettings(settings);
     const obj = settings.toJson();
     await chromeSet("settings", obj);
@@ -41,7 +45,7 @@ const OptionsPage: React.FC = () => {
     <Stack>
       <Stack horizontalAlign="center">
         <h1>HYPERTRONS</h1>
-        <sub>version 0.1.2</sub>
+        <sub>{`version ${version}`}</sub>
       </Stack>
       <Stack horizontalAlign="center">
         <div className="container">
@@ -58,38 +62,6 @@ const OptionsPage: React.FC = () => {
                   childrenGap: 10
                 }}
               >
-                <Checkbox
-                  label={getMessageI18n("component_developerCollabrationNetwork_title")}
-                  defaultChecked={settings.developerNetwork}
-                  onChange={async (e, checked) => {
-                    settings.developerNetwork = checked;
-                    await saveSettings();
-                  }}
-                />
-                <Checkbox
-                  label={getMessageI18n("component_projectCorrelationNetwork_title")}
-                  defaultChecked={settings.projectNetwork}
-                  onChange={async (e, checked) => {
-                    settings.projectNetwork = checked;
-                    await saveSettings();
-                  }}
-                />
-                <Stack
-                  horizontalAlign="start"
-                  verticalAlign='center'
-                  horizontal
-                  tokens={{
-                    childrenGap: 10
-                  }}
-                >
-                  <DefaultButton
-                    style={{ width: 100 }}
-                    onClick={() => {
-                    }}
-                  >
-                    {getMessageI18n("global_btn_ok")}
-                  </DefaultButton>
-                </Stack>
                 <Toggle
                   label={getMessageI18n('options_toggle_checkForUpdates')}
                   defaultChecked={settings.checkForUpdates}
@@ -97,9 +69,26 @@ const OptionsPage: React.FC = () => {
                   offText={getMessageI18n('options_toggle_checkForUpdates_offText')}
                   onChange={async (e, checked) => {
                     settings.checkForUpdates = checked;
-                    await saveSettings();
+                    await saveSettings(settings);
                   }}
                 />
+                <Checkbox
+                  label={getMessageI18n("component_developerCollabrationNetwork_title")}
+                  defaultChecked={settings.developerNetwork}
+                  onChange={async (e, checked) => {
+                    settings.developerNetwork = checked;
+                    await saveSettings(settings);
+                  }}
+                />
+                <Checkbox
+                  label={getMessageI18n("component_projectCorrelationNetwork_title")}
+                  defaultChecked={settings.projectNetwork}
+                  onChange={async (e, checked) => {
+                    settings.projectNetwork = checked;
+                    await saveSettings(settings);
+                  }}
+                />
+
 
               </Stack>
             </PivotItem>
--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,12 +1,43 @@
+import { compareVersion, getBrowserType} from '../../utils/utils';
+import { getUpdateInfor } from "../../services/background"
+import { loadSettings } from '../../utils/settings';
+
 export enum BackgroundTasks {
   update = 'check_for_updates'
 }
 
-chrome.alarms.create(BackgroundTasks.update,{periodInMinutes:10});
+chrome.alarms.create(BackgroundTasks.update,{periodInMinutes:0.1});
 
-chrome.alarms.onAlarm.addListener((alarm) => {
+chrome.alarms.onAlarm.addListener(async (alarm) => {
   const name=alarm.name;
-  if(name===BackgroundTasks.update){
-    console.log("check for updates");
+
+  const settings=await loadSettings();
+
+  if(name===BackgroundTasks.update ){
+    if(settings.checkForUpdates){
+      // @ts-ignore :we must ignore here
+      const details=chrome.app.getDetails();
+      const currentVersion=details["version"];
+      const browserType=getBrowserType();
+      const updateInformation=await getUpdateInfor();
+      let latestVersion;
+
+      if("key" in details){
+        // the store-version installation
+        if(browserType==="Edge"){
+          latestVersion=updateInformation["edge"]["latest_version"];
+        }
+        else{
+          latestVersion=updateInformation["chrome"]["latest_version"];
+        }
+      }
+      else{
+        latestVersion=updateInformation["develop"]["latest_version"];
+      }
+
+      if(compareVersion(currentVersion,latestVersion)===-1){
+        console.log(`new ${browserType} update available, version:${latestVersion}`);
+      }
+    }
   }
 });
