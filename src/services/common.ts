2c95c74da2d476aee447b241b3a499fdf5a628bd
LiuChangFreeman <liuchangfreeman@live.com>
2021-04-18 23:55:06 +0800

    support for github token

--- src/services/common.ts
+++ src/services/common.ts
@@ -27,3 +27,8 @@ export const checkUpdate= async ()=>{
   }
   return [currentVersion,latestVersion,updateUrl];
 }
+
+export const checkIsTokenAvailabe= async (token:string)=>{
+  const response = await fetch(`https://api.github.com/user`,{headers: { "Authorization":`token ${token}` }});
+  return await response.json();
+}
