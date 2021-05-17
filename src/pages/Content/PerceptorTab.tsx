73d9e9ef47bcf854cca50092a6e53b0bafb0c85d
LiuChangFreeman <liuchangfreeman@live.com>
2021-05-17 21:11:07 +0800

    add teachingBubble to PerceptorTab

--- src/pages/Content/PerceptorTab.tsx
+++ src/pages/Content/PerceptorTab.tsx
@@ -4,14 +4,19 @@ import { utils } from 'github-url-detection';
 import { isPerceptor, runsWhen } from '../../utils/utils';
 import PerceptorBase from './PerceptorBase';
 import { inject2Perceptor } from './Perceptor';
+import { render } from 'react-dom';
+import React from 'react';
+import TeachingBubbleWrapper from './TeachingBubbleWrapper'
 
 @runsWhen([pageDetect.isRepo])
 class PerceptorTab extends PerceptorBase {
+
   public async run(): Promise<void> {
     const insightsTab = $('.js-repo-nav [data-ga-click="Repository, Navigation click, Insights tab"]').parent();
 
     // copy Insights tab
     const perceptorTab = insightsTab.clone(true);
+    perceptorTab.attr('id','perceptor_tab');
 
     // Un-select one of the tabs if necessary
     const insightsLink = $('a', insightsTab);
@@ -27,10 +32,20 @@ class PerceptorTab extends PerceptorBase {
       }
     }
 
+    // Deal with teaching bubble
+    const teaching_bubble = document.createElement('div');
+    $('.js-repo-pjax-container').prepend(teaching_bubble);
+
     // Update
     perceptorLink.attr("href", `https://github.com/${utils.getRepositoryInfo(window.location)!.nameWithOwner}/pulse?redirect=perceptor`);
     $('span[data-content="Insights"]', perceptorLink).text('Perceptor');
 
+
+    render(
+      <TeachingBubbleWrapper target="#perceptor_tab"/>
+      ,
+      teaching_bubble
+    );
     insightsTab.after(perceptorTab);
   }
 }
--- src/pages/Content/ProjectBase.tsx
+++ src/pages/Content/ProjectBase.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Graph from '../../components/Graph/Graph';
 import {
-  Dialog,DialogType, IButtonProps,Image,TeachingBubble
+  Dialog,DialogType,Image
 } from 'office-ui-fabric-react';
-import { useBoolean } from '@fluentui/react-hooks';
-import { chromeSet, getMessageI18n } from '../../utils/utils';
-import MetaData, { loadMetaData } from '../../utils/metadata';
+import { getMessageI18n } from '../../utils/utils';
+import TeachingBubbleWrapper from './TeachingBubbleWrapper'
 
 export interface ProjectBaseProps {
   graphType:string,
@@ -18,95 +17,52 @@ const ProjectBase: React.FC<ProjectBaseProps> =
      graphType,
      developerCollabrationData,
      participatedProjectsData,
- }) => {
-  const [metaData, setMetaData] = useState(new MetaData());
-  const [showDialog, setShowDialog] = useState(false);
-  const [inited, setInited] = useState(false);
-  const [teachingBubbleVisible, { toggle: toggleTeachingBubbleVisible }] = useBoolean(true);
-  useEffect(() => {
-    const initMetaData = async () => {
-      const temp=await loadMetaData();
-      setMetaData(temp);
-      setInited(true);
-    }
-    initMetaData();
-  }, []);
-  
-  const disableButtonProps: IButtonProps = React.useMemo(
-    () => ({
-      children: getMessageI18n('teachingBubble_text_disable'),
-      onClick: async ()=>{
-        metaData.showTeachingBubble=false;
-        await chromeSet("meta_data", metaData.toJson());
-        toggleTeachingBubbleVisible();
-      },
-    }),
-    [metaData, toggleTeachingBubbleVisible],
-  );
-  
-  const confirmButtonProps: IButtonProps = React.useMemo(
-    () => ({
-      children: getMessageI18n("teachingBubble_text_ok"),
-      onClick: toggleTeachingBubbleVisible,
-    }),
-    [toggleTeachingBubbleVisible],
-  );
+   }) => {
 
-  return (
-    <div
-      style={{
-        marginTop:10
-      }}
-    >
-      <h2 className="f4 mb-2 text-normal">
-        Hypertrons network
-      </h2>
-      <Image
-        id="charts_icon"
-        src={chrome.runtime.getURL("charts.png")}
-        height={50}
-        width={50}
-        onClick={()=>{
-          setShowDialog(true)
-        }}
-      />
-      {
-        teachingBubbleVisible &&metaData.showTeachingBubble &&inited &&
-        <TeachingBubble
-          target="#charts_icon"
-          primaryButtonProps={disableButtonProps}
-          secondaryButtonProps={confirmButtonProps}
-          onDismiss={toggleTeachingBubbleVisible}
-          headline={getMessageI18n('teachingBubble_text_headline')}
-        >
-          {getMessageI18n('teachingBubble_text_content')}
-        </TeachingBubble>
-      }
-      <Dialog
-        hidden={!showDialog}
-        onDismiss={() => {
-          setShowDialog(false);
-        }}
-        dialogContentProps={{
-          type: DialogType.normal,
-          title: getMessageI18n("component_projectNetwork_title")
-        }}
+    const [showDialog, setShowDialog] = useState(false);
+
+    return (
+      <div
+        className="border-top color-border-secondary pt-3 mt-3"
       >
-        <div >
-          < Graph
-            title={getMessageI18n('component_developerCollabrationNetwork_title')}
-            graphType={graphType}
-            data={developerCollabrationData}
-          />
-          < Graph
-            title={getMessageI18n('component_mostParticipatedProjects_title')}
-            graphType={graphType}
-            data={participatedProjectsData}
-          />
-        </div>
-      </Dialog>
-    </div>
-  )
-};
+        <h2 className="h4 mb-2">
+          Hypertrons
+        </h2>
+        <Image
+          id="charts_icon"
+          src={chrome.runtime.getURL("charts.png")}
+          height={50}
+          width={50}
+          onClick={()=>{
+            setShowDialog(true)
+          }}
+        />
+        <TeachingBubbleWrapper target="#charts_icon"/>
+        <Dialog
+          hidden={!showDialog}
+          onDismiss={() => {
+            setShowDialog(false);
+          }}
+          dialogContentProps={{
+            type: DialogType.normal,
+            title: getMessageI18n("component_projectNetwork_title")
+          }}
+        >
+          <div >
+            < Graph
+              title={getMessageI18n('component_developerCollabrationNetwork_title')}
+              graphType={graphType}
+              data={developerCollabrationData}
+            />
+            < Graph
+              title={getMessageI18n('component_mostParticipatedProjects_title')}
+              graphType={graphType}
+              data={participatedProjectsData}
+            />
+          </div>
+        </Dialog>
+      </div>
+    )
+  };
 
 export default ProjectBase;
