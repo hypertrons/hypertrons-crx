d9e7982e9d9f8af06554a55a4b5743eee4f68882
Ayush PS <ayushpratap16@gmail.com>
2021-06-18 02:42:14 +0530

    Possible Icon change
    Signed-off-by: Ayush PS <ayushpratap16@gmail.com>

--- src/pages/Content/PerceptorTab.tsx
+++ src/pages/Content/PerceptorTab.tsx
@@ -61,6 +61,9 @@ class PerceptorTab extends PerceptorBase {
     // Update
     perceptorLink.attr("href", `https://github.com/${utils.getRepositoryInfo(window.location)!.nameWithOwner}/pulse?redirect=perceptor`);
     $('span[data-content="Insights"]', perceptorLink).text('Perceptor');
+    $('svg.octicon', perceptorLink).html('<path fill-rule="evenodd" d="M0.5 1.75a.75.75 0 00-1.5 -2v12.5c0 .414.336.75.75.75h14.5a.75.75 0 000-1.5H1.5V1.75zm14.28 2.53a.75.75 0 00-1.06-1.06L10 7.94 7.53 5.47a.75.75 0 00-1.06 0L3.22 8.72a.75.75 0 001.06 1.06L7  2.47a.75.75 0 001.06 0l5.25-5.25z"></path>');
+    //Need to add some icon here {Currently it's just distorted insights icon}
+
 
 
     render(
