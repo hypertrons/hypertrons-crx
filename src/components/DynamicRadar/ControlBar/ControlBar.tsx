6b4126310351b7460efcc93b0331098b2c876be0
tyn1998 <32434520+tyn1998@users.noreply.github.com>
2021-08-13 08:50:00 +0800

    feat: Add DynamicRadar component. (#207)
    This component consists of 3 sub components:
     - Radar
     - SimpleTable
     - ControlBar

---
+++ src/components/DynamicRadar/ControlBar/ControlBar.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { Stack, Slider, initializeIcons, IconButton } from '@fluentui/react';
+import {
+  IStyleFunctionOrObject,
+  ISliderStyleProps,
+  ISliderStyles,
+} from '@fluentui/react';
+
+initializeIcons();
+
+interface ControlBarProps {
+  theme: 'light' | 'dark';
+  play: Function;
+  pause: Function;
+  setIndex: Function;
+  sliderValue: number;
+  sliderMax: number;
+}
+
+const ControlBar: React.FC<ControlBarProps> = (props) => {
+  const { theme, play, pause, setIndex, sliderValue, sliderMax } = props;
+  const [ifPlay, setIfPlay] = useState(false);
+
+  const sliderStyle: IStyleFunctionOrObject<ISliderStyleProps, ISliderStyles> =
+    {
+      thumb: {
+        background: theme == 'light' ? 'darkblue' : '#58a6ff',
+        borderRadius: 0,
+        height: '14px',
+        top: '-5px',
+        width: '6px',
+        borderWidth: '0px',
+      },
+      activeSection: {
+        background: theme == 'light' ? 'rgb(200, 198, 196)' : '#58a6ff',
+      },
+      inactiveSection: {
+        background: theme == 'light' ? 'rgb(237, 235, 233)' : 'white',
+      },
+    };
+
+  const previous = () => {
+    setIndex((idx: number) => (idx - 1 < 0 ? 0 : idx - 1));
+  };
+
+  const next = () => {
+    setIndex((idx: number) => (idx + 1 > sliderMax ? sliderMax : idx + 1));
+  };
+
+  return (
+    <div style={{ width: '91%', marginLeft: '9%' }}>
+      <Stack horizontal>
+        <IconButton
+          iconProps={{
+            iconName: ifPlay ? 'CirclePauseSolid' : 'MSNVideosSolid',
+          }}
+          styles={{
+            icon: { color: theme == 'light' ? 'darkblue' : '#58a6ff' },
+          }}
+          onClick={() => {
+            ifPlay ? pause() : play();
+            setIfPlay(!ifPlay);
+          }}
+        />
+        <IconButton
+          iconProps={{ iconName: 'ChevronLeftSmall' }}
+          styles={{
+            icon: { color: theme == 'light' ? 'darkblue' : '#58a6ff' },
+          }}
+          onClick={previous}
+        />
+        <Stack.Item grow align="center">
+          <Slider
+            styles={sliderStyle}
+            min={0}
+            max={sliderMax}
+            step={1}
+            value={sliderValue}
+            showValue={false}
+            snapToStep
+            onChange={(value) => setIndex(value)}
+          />
+        </Stack.Item>
+        <IconButton
+          iconProps={{ iconName: 'ChevronRightSmall' }}
+          styles={{
+            icon: { color: theme == 'light' ? 'darkblue' : '#58a6ff' },
+          }}
+          onClick={next}
+        />
+      </Stack>
+    </div>
+  );
+};
+
+export default ControlBar;
---
+++ src/components/DynamicRadar/Radar/Radar.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
+
+interface RadarProps {
+  theme: 'light' | 'dark';
+  height: number;
+  indicators: string[];
+  maxScales: number[];
+  values: number[];
+}
+
+const Radar: React.FC<RadarProps> = (props) => {
+  const { theme, height, indicators, maxScales, values } = props;
+  const divEL = useRef(null);
+
+  const option = {
+    legend: undefined,
+    tooltip: undefined,
+    radar: {
+      shape: 'circle',
+      indicator: indicators.map((item: any, index: any) => {
+        return { name: item, max: maxScales[index] };
+      }),
+      center: ['50%', '50%'],
+      splitNumber: 3,
+      name: {
+        textStyle: {
+          color: theme === 'light' ? '#010409' : '#f0f6fc',
+          fontSize: 13,
+          textShadowColor: theme === 'light' ? '#010409' : '#f0f6fc',
+          textShadowBlur: 0.01,
+          textShadowOffsetX: 0.01,
+          textShadowOffsetY: 0.01,
+        },
+      },
+      splitArea: {
+        areaStyle: {
+          color: [theme == 'light' ? 'darkblue' : '#58a6ff'],
+          opacity: 1,
+        },
+      },
+      axisLine: {
+        lineStyle: {
+          width: 2,
+          color: 'white',
+          opacity: 0.4,
+        },
+      },
+      splitLine: {
+        lineStyle: {
+          color: 'white',
+          width: 1.5,
+          opacity: 0.4,
+        },
+      },
+    },
+    series: [
+      {
+        type: 'radar',
+        symbol: 'none',
+        itemStyle: {},
+        lineStyle: {
+          width: 1.5,
+          color: 'white',
+          opacity: 1,
+          shadowBlur: 4,
+          shadowColor: 'white',
+          shadowOffsetX: 0.5,
+          shadowOffsetY: 0.5,
+        },
+        areaStyle: {
+          color: 'white',
+          opacity: 0.8,
+        },
+        data: [
+          {
+            value: values,
+          },
+        ],
+      },
+    ],
+    animation: true,
+    animationDurationUpdate: 200,
+  };
+  useEffect(() => {
+    let chartDOM = divEL.current;
+    const instance = echarts.init(chartDOM as any);
+
+    return () => {
+      instance.dispose();
+    };
+  }, []);
+
+  useEffect(() => {
+    let chartDOM = divEL.current;
+    const instance = echarts.getInstanceByDom(chartDOM as any);
+    instance.setOption(option);
+  }, [indicators, values]);
+
+  return <div ref={divEL} style={{ width: '100%', height }}></div>;
+};
+
+export default Radar;
