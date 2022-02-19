043d40de1d855b58fa2cb38b0697001370a306cf
Lam Tang <tangyenan@gmail.com>
2022-02-19 10:25:04 +0800

    [refactor] modify crx-webpack-plugin to make it compatible with webpack5 hooks API

--- utils/build.js
+++ utils/build.js
@@ -3,10 +3,22 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
 
+const path = require('path');
+const CrxWebpackPlugin = require('./crx-webpack-plugin/index');
+
 var webpack = require('webpack'),
   config = require('../webpack.config');
 
 delete config.custom;
+
+config.plugins.push(
+  new CrxWebpackPlugin({
+    keyFile: path.resolve(__dirname, '../build.pem'),
+    contentPath: path.resolve(__dirname, '../build'),
+    outputPath: path.resolve(__dirname, '../release'),
+    name: 'hypercrx',
+  })
+);
 
 config.mode = 'production';
 
---
+++ utils/crx-webpack-plugin/index.js
@@ -0,0 +1,81 @@
+var fs = require('fs');
+var path = require('path');
+var join = path.join;
+var mkdirp = require('mkdirp');
+var ChromeExtension = require('crx');
+
+function CrxWebpackPlugin(options) {
+  this.options = options || {};
+  if (!this.options.updateUrl) {
+    this.options.updateUrl = "http://localhost:8000/";
+  }
+  if (!this.options.updateFilename) {
+    this.options.updateFilename = "updates.xml";
+  }
+
+  // remove trailing slash
+  this.options.updateUrl = this.options.updateUrl.replace(/\/$/, "");
+
+  // setup paths
+  this.context = path.dirname(module.parent.filename);
+  this.keyFile = path.isAbsolute(this.options.keyFile) ? this.options.keyFile : join(this.context, this.options.keyFile);
+  this.outputPath = path.isAbsolute(this.options.outputPath) ? this.options.outputPath : join(this.context, this.options.outputPath);
+  this.contentPath = path.isAbsolute(this.options.contentPath) ? this.options.contentPath : join(this.context, this.options.contentPath);
+
+  // set output info
+  this.crxName = this.options.name + ".crx";
+  this.crxFile = join(this.outputPath, this.crxName);
+  this.updateFile = join(this.outputPath, this.options.updateFilename);
+  this.updateUrl = this.options.updateUrl + "/" + this.options.updateFilename;
+
+  // initiate crx
+  this.crx = new ChromeExtension({
+    privateKey: fs.readFileSync(this.keyFile),
+    codebase: this.options.updateUrl + '/' + this.crxName
+  });
+}
+
+// hook into webpack
+CrxWebpackPlugin.prototype.apply = function(compiler) {
+  var self = this;
+  self.logger = compiler.getInfrastructureLogger('crx-webpack-plugin');
+  return compiler.hooks.done.tap('crx-webpack-plugin', function() {
+    self.package.call(self);
+  })
+}
+
+// package the extension
+CrxWebpackPlugin.prototype.package = function() {
+  var self = this;
+  self.crx.load(self.contentPath).then(function() {
+    self.crx.pack().then(function(buffer) {
+      mkdirp(self.outputPath, function(err) {
+        if (err)
+        {
+          self.logger.error(err);
+          throw err
+        }
+
+        var updateXML = self.crx.generateUpdateXML();
+        fs.writeFile(self.updateFile, updateXML, function(err) {
+          if (err) {
+            self.logger.error(err);
+            throw err
+          }
+
+          self.logger.info('wrote updateFile to ' + self.updateFile);
+          fs.writeFile(self.crxFile, buffer, function(err) {
+            if (err) {
+              self.logger.error(err);
+              throw err;
+            }
+
+            self.logger.info('wrote crxFile to ' + self.crxFile);
+          });
+        });
+      });
+    });
+  });
+}
+
+module.exports = CrxWebpackPlugin;
