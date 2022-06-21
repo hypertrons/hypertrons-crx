4e0ed6b91e7a07245656440e609cd35d5eb124f8
nzcer <ningzhicheng1215@qq.com>
2022-06-21 19:13:29 +0800

    chore: update new issues url

--- src/constant.ts
+++ src/constant.ts
@@ -9,7 +9,7 @@ export const HYPERTRONS_OSS_XLAB_ENDPOINT = 'https://hypertrons-oss.x-lab.info';
 export const OSS_XLAB_ENDPOINT = 'https://oss.x-lab.info';
 
 export const HYPERTRONS_CRX_NEW_ISSUE =
-  'https://github.com/hypertrons/hypertrons-crx/issues/new';
+  'https://github.com/hypertrons/hypertrons-crx/issues/new/choose';
 
 // export const ACTIVITY_DEFINITION_LINK = 'https://github.com/X-lab2017/open-digger/';
 
