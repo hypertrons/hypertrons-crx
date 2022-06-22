0464b684f9ddabe528bc83afc49dde19cba8cd1e
Lam Tang <tangyenan@gmail.com>
2022-06-22 14:22:59 +0800

    feat: automatically upload and publish the extension to chrome webstore
    chore: prettier

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,53 +1,29 @@
-const zipFolder = require('zip-folder');
+import chromeWebstoreUpload from 'chrome-webstore-upload';
 const fs = require('fs');
 
-let folderName = '../build/';
-let zipName = '../release/hypercrx.zip';
+const ZIP_PATH = '../release/hypercrx.zip';
 
 // getting all the credentials and IDs from environment
-let REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-let EXTENSION_ID = process.env.EXTENSION_ID;
-let CLIENT_SECRET = process.env.CLIENT_SECRET;
-let CLIENT_ID = process.env.CLIENT_ID;
+let REFRESH_TOKEN = process.env.CHROME_REFRESH_TOKEN;
+let EXTENSION_ID = process.env.CHROME_EXTENSION_ID;
+let CLIENT_SECRET = process.env.CHROME_CLIENT_SECRET;
+let CLIENT_ID = process.env.CHROME_CLIENT_ID;
 
-const webStore = require('chrome-webstore-upload')({
+const store = chromeWebstoreUpload({
   extensionId: EXTENSION_ID,
   clientId: CLIENT_ID,
   refreshToken: REFRESH_TOKEN,
   clientSecret: CLIENT_SECRET,
 });
 
-function upload() {
-  const extesnionSource = fs.createReadStream(zipName);
-  webStore
-    .uploadExisting(extesnionSource)
-    .then((res) => {
-      console.log('Successfully uploaded the ZIP');
-      webStore
-      .publish()
-      .then((res) => {
-        console.log('Successfully published the newer version');
-      })
-      .catch((error) => {
-        console.log(`Error while publishing uploaded extension: ${error}`);
-        process.exit(1);
-      });
-    })
-    .catch((error) => {
-      console.log(`Error while uploading ZIP: ${error}`);
-      process.exit(1);
-    });
-}
+const zipFile = fs.createReadStream(ZIP_PATH);
 
-zipFolder(folderName, zipName, function (err) {
-  if (err) {
-    console.log('oh no! ', err);
-  } else {
-    console.log(
-      `Successfully zipped the ${folderName} directory and store as ${zipName}`
-    );
-    // will be invoking upload process
-    upload();
-  }
-});
+(async () => {
+  const token = await store.fetchToken();
 
+  const uploadRes = await store.uploadExisting(zipFile, token);
+  console.log({ uploadRes });
+
+  const publishRes = await store.publish('default', token);
+  console.log({ publishRes });
+})();
