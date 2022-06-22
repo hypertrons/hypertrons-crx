89b4c3b2dabe40960e014c40fdecaac56149cab8
Lam Tang <tangyenan@gmail.com>
2022-06-22 16:07:20 +0800

    fix: variable names inconsistence

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const ZIP_PATH = '../release/hypercrx.zip';
 
 // getting all the credentials and IDs from environment
-let REFRESH_TOKEN = process.env.CHROME_REFRESH_TOKEN;
-let EXTENSION_ID = process.env.CHROME_EXTENSION_ID;
-let CLIENT_SECRET = process.env.CHROME_CLIENT_SECRET;
-let CLIENT_ID = process.env.CHROME_CLIENT_ID;
+const CHROME_EXTENSION_ID = process.env.CHROME_EXTENSION_ID;
+const CHROME_CLIENT_ID = process.env.CHROME_CLIENT_ID;
+const CHROME_CLIENT_SECRET = process.env.CHROME_CLIENT_SECRET;
+const CHROME_REFRESH_TOKEN = process.env.CHROME_REFRESH_TOKEN;
 
 const store = chromeWebstoreUpload({
-  extensionId: EXTENSION_ID,
-  clientId: CLIENT_ID,
-  refreshToken: REFRESH_TOKEN,
-  clientSecret: CLIENT_SECRET,
+  extensionId: CHROME_EXTENSION_ID,
+  clientId: CHROME_CLIENT_ID,
+  clientSecret: CHROME_CLIENT_SECRET,
+  refreshToken: CHROME_REFRESH_TOKEN,
 });
 
 const zipFile = fs.createReadStream(ZIP_PATH);
