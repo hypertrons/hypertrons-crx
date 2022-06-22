6fb8f5b04d2970472a4950d4a7e4c91714e20bac
wxh <xiaohanwu12@gmail.com>
2022-06-22 03:00:41 +0000

    refactor deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,5 @@
-import zipFolder from 'zip-folder';
-import chromeWebstoreUpload from 'chrome-webstore-upload';
-import fs from 'fs';
+const zipFolder = require('zip-folder');
+const fs = require('fs');
 
 let folderName = '../build/';
 let zipName = '../release/hypercrx.zip';
@@ -11,7 +10,7 @@ let EXTENSION_ID = process.env.EXTENSION_ID;
 let CLIENT_SECRET = process.env.CLIENT_SECRET;
 let CLIENT_ID = process.env.CLIENT_ID;
 
-const webStore = chromeWebstoreUpload({
+const webStore = require('chrome-webstore-upload')({
   extensionId: EXTENSION_ID,
   clientId: CLIENT_ID,
   refreshToken: REFRESH_TOKEN,
@@ -24,24 +23,18 @@ function upload() {
     .uploadExisting(extesnionSource)
     .then((res) => {
       console.log('Successfully uploaded the ZIP');
-
-      // call publish API on success
+      webStore
+      .publish()
+      .then((res) => {
+        console.log('Successfully published the newer version');
+      })
+      .catch((error) => {
+        console.log(`Error while publishing uploaded extension: ${error}`);
+        process.exit(1);
+      });
     })
     .catch((error) => {
       console.log(`Error while uploading ZIP: ${error}`);
-      process.exit(1);
-    });
-}
-
-function publish() {
-  // publish the uploaded zip
-  webStore
-    .publish()
-    .then((res) => {
-      console.log('Successfully published the newer version');
-    })
-    .catch((error) => {
-      console.log(`Error while publishing uploaded extension: ${error}`);
       process.exit(1);
     });
 }
@@ -54,7 +47,7 @@ zipFolder(folderName, zipName, function (err) {
       `Successfully zipped the ${folderName} directory and store as ${zipName}`
     );
     // will be invoking upload process
+    upload();
   }
 });
-upload();
-publish();
+
