791c261fc46b2be03dfd1868c498dfbb9a172fb6
Lam Tang <tangyenan@gmail.com>
2022-07-07 14:14:22 +0800

    fix: exsiting way to write process.env.XXX does not work

--- utils/build.js
+++ utils/build.js
@@ -1,8 +1,3 @@
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
-process.env.ASSET_PATH = '/';
-
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import CrxWebpackPlugin from './crx-webpack-plugin/index.js';
--- utils/server.js
+++ utils/server.js
@@ -1,8 +1,3 @@
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
-process.env.ASSET_PATH = '/';
-
 import WebpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
 import config from '../webpack.config.js';
