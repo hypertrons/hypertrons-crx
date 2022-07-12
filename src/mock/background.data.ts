da15e539b7cff40138569ac926bb39ba972d6e1d
Conventional Changelog Action <conventional.changelog.action@github.com>
2022-07-12 02:57:21 +0000

    chore(release): v1.6.2 [skip ci]

--- src/mock/background.data.ts
+++ src/mock/background.data.ts
@@ -1,14 +1,14 @@
 export const updateInformation = {
   chrome: {
-    latest_version: '1.6.1',
+    latest_version: '1.6.2',
     url: 'https://github.com/hypertrons/hypertrons-crx/releases',
   },
   edge: {
-    latest_version: '1.6.1',
+    latest_version: '1.6.2',
     url: 'https://github.com/hypertrons/hypertrons-crx/releases',
   },
   develop: {
-    latest_version: '1.6.1',
+    latest_version: '1.6.2',
     url: 'https://github.com/hypertrons/hypertrons-crx/releases',
   },
 };
