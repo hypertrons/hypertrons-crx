c421899e383fcbfcc381ca7e4767b3561a2d608b
Lam Tang <tangyenan@gmail.com>
2022-11-06 15:54:16 +0800

    fix: bars of ActiInflTrend Chart not respond to click when language is English

--- src/views/RepoActiInflTrendView/RepoActiInflTrendView.tsx
+++ src/views/RepoActiInflTrendView/RepoActiInflTrendView.tsx
@@ -61,12 +61,8 @@ const RepoActiInflTrendView: React.FC<RepoActiInflTrendViewProps> = ({
   let barsData: any = generateBarsData(repoActiInflData);
 
   const onClick = (params: any) => {
-    const { seriesName, data } = params;
-    const yName = getMessageByLocale(
-      'component_repoActiInflTrend_yName1',
-      settings.locale
-    );
-    if (seriesName === yName) {
+    const { seriesIndex, data } = params;
+    if (seriesIndex === 0) {
       let [year, month] = data.toString().split(',')[0].split('-');
       if (month.length < 2) {
         month = '0' + month;
