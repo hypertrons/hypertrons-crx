b38882c652b6944f1d26e72389be63d7ea8c023a
Lam Tang <tangyenan@gmail.com>
2022-11-15 16:12:30 +0800

    refactor: use new API to refactor developerActInflTrend

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -20,8 +20,7 @@ export const generateDataByMonth = (originalData: any) => {
   const arrayData: [string, number][] = [];
   const start = new Date(oldestMonth);
   const end = new Date(newestMonth);
-  end.setMonth(end.getMonth() + 1); // if not +1 month, i <= end will miss the last month occasionally
-  for (let i = start; i < end; i.setMonth(i.getMonth() + 1)) {
+  for (let i = start; i <= end; i.setMonth(i.getMonth() + 1)) {
     const date = i.getFullYear() + '-' + (i.getMonth() + 1);
     if (!objectData.hasOwnProperty(date)) {
       arrayData.push([date, 0]);
