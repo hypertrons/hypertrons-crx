fce7103a90b79f2dc457a6973849d5a8262ab728
Lam Tang <tangyenan@gmail.com>
2022-12-06 16:28:11 +0800

    a minimal feature-manager.ts (possible, I'm not sure)

---
+++ src/feature-manager.ts
@@ -0,0 +1,168 @@
+import select from 'select-dom';
+import domLoaded from 'dom-loaded';
+import stripIndent from 'strip-indent';
+import { Promisable } from 'type-fest';
+import * as pageDetect from 'github-url-detection';
+
+import waitFor from './helpers/wait-for';
+import { shouldFeatureRun } from './github-helpers';
+
+type BooleanFunction = () => boolean;
+type FeatureInit = () => Promisable<void>;
+
+type FeatureLoader = {
+  /** Whether to wait for DOM ready before running `init`. `false` makes `init` run right as soon as `body` is found.
+   * @default true
+   */
+  awaitDomReady?: boolean;
+
+  /** When pressing the back button, DOM changes and listeners are still there. Using a selector here would use the integrated deduplication logic.
+   * @default false
+   */
+  deduplicate?: string;
+
+  init: FeatureInit; // Repeated here because this interface is Partial<>
+} & Partial<InternalRunConfig>;
+
+type InternalRunConfig = {
+  asLongAs: BooleanFunction[] | undefined;
+  include: BooleanFunction[] | undefined;
+  exclude: BooleanFunction[] | undefined;
+  init: FeatureInit;
+};
+
+const { version } = browser.runtime.getManifest();
+
+const logError = (url: string, error: unknown): void => {
+  const id = getFeatureID(url);
+  const message = error instanceof Error ? error.message : String(error);
+
+  if (message.includes('token')) {
+    console.log('ℹ️', id, '→', message);
+    return;
+  }
+
+  // Don't change this to `throw Error` because Firefox doesn't show extensions' errors in the console
+  console.group(`❌ ${id}`); // Safari supports only one parameter
+  console.log(`📕 ${version} →`, error); // One parameter improves Safari formatting
+  console.groupEnd();
+};
+
+const log = {
+  info: console.log,
+  http: console.log,
+  error: logError,
+};
+
+// eslint-disable-next-line no-async-promise-executor -- Rule assumes we don't want to leave it pending
+const globalReady = new Promise<void>(async (resolve) => {
+  await waitFor(() => document.body);
+
+  if (pageDetect.is500() || pageDetect.isPasswordConfirmation()) {
+    return;
+  }
+
+  if (select.exists('html.hypercrx')) {
+    console.warn(
+      stripIndent(`
+      Hypercrx has been loaded twice. This may be because:
+
+      • You also loaded the developer version
+
+      If you see this at every load, please open an issue in our repository.`)
+    );
+    return;
+  }
+
+  document.documentElement.classList.add('hypercrx');
+
+  resolve();
+});
+
+const setupPageLoad = async (
+  id: FeatureID,
+  config: InternalRunConfig
+): Promise<void> => {
+  const { asLongAs, include, exclude, init } = config;
+
+  if (!shouldFeatureRun({ asLongAs, include, exclude })) {
+    return;
+  }
+
+  const runFeature = async (): Promise<void> => {
+    try {
+      await init();
+      log.info('✅', id);
+    } catch (error) {
+      log.error(id, error);
+    }
+  };
+
+  await runFeature();
+};
+
+const getFeatureID = (url: string): FeatureID =>
+  url.split('/').pop()!.split('.')[0] as FeatureID;
+
+/** Register a new feature */
+const add = async (url: string, ...loaders: FeatureLoader[]): Promise<void> => {
+  const id = getFeatureID(url);
+  /* Feature filtering and running */
+  await globalReady;
+
+  for (const loader of loaders) {
+    // Input defaults and validation
+    const {
+      asLongAs,
+      include,
+      exclude,
+      init,
+      awaitDomReady = true,
+      deduplicate = false,
+    } = loader;
+
+    if (include?.length === 0) {
+      throw new Error(
+        `${id}: \`include\` cannot be an empty array, it means "run nowhere"`
+      );
+    }
+
+    // 404 pages should only run 404-only features
+    if (
+      pageDetect.is404() &&
+      !include?.includes(pageDetect.is404) &&
+      !asLongAs?.includes(pageDetect.is404)
+    ) {
+      continue;
+    }
+
+    const details = {
+      asLongAs,
+      include,
+      exclude,
+      init,
+    };
+    if (awaitDomReady) {
+      (async () => {
+        await domLoaded;
+        await setupPageLoad(id, details);
+      })();
+    } else {
+      void setupPageLoad(id, details);
+    }
+
+    document.addEventListener('turbo:render', () => {
+      if (!deduplicate || !select.exists(deduplicate)) {
+        void setupPageLoad(id, details);
+      }
+    });
+  }
+};
+
+const features = {
+  add,
+  log,
+  getFeatureID,
+};
+
+export default features;
--- src/global.d.ts
+++ src/global.d.ts
@@ -42,3 +42,26 @@ interface IGraphData {
 type NodeClickFunc = (node: INode) => void;
 
 type ThemeType = 'light' | 'dark';
+
+type DeinitHandle =
+  | { disconnect: VoidFunction }
+  | { clear: VoidFunction }
+  | { destroy: VoidFunction }
+  | { abort: VoidFunction }
+  | VoidFunction;
+type Deinit = DeinitHandle | DeinitHandle[];
+
+type FeatureID = string & { feature: true };
+interface FeatureMeta {
+  id: FeatureID;
+  description: string;
+  screenshot?: string;
+}
+
+declare namespace JSX {
+  /* eslint-disable @typescript-eslint/no-redundant-type-constituents -- https://github.com/refined-github/refined-github/pull/5654#discussion_r878891540 */
+  interface IntrinsicElements {
+    'has-hypercrx': IntrinsicElements.div;
+    'has-hypercrx-inner': IntrinsicElements.div;
+  }
+}
