9abf2576299ae2afb5193f7a1797932499067e45
Lam Tang <tangyenan@gmail.com>
2023-02-08 14:26:27 +0800

    refactor: a more clear init() and restore()

--- src/pages/ContentScripts/features/repo-fork-tooltip/index.tsx
+++ src/pages/ContentScripts/features/repo-fork-tooltip/index.tsx
@@ -6,7 +6,6 @@ import $ from 'jquery';
 import features from '../../../../feature-manager';
 import { isPublicRepo, getGithubTheme } from '../../../../utils/utils';
 import { getRepoName } from '../../../../helpers/get-repo-info';
-import exists from '../../../../helpers/exists';
 import { getForks } from '../../../../api/repo';
 import View from './view';
 
@@ -23,20 +22,15 @@ const getData = async () => {
   }
 };
 
-const attach = async () => {
+const renderTo = (container: Container) => {
+  render(<View forks={forks} />, container);
+};
+
+const init = async (): Promise<void> => {
+  repoName = getRepoName();
+  await getData();
+
   await elementReady('a[data-ga-click*="show fork modal"]');
-
-  // Ideally, we should do nothing if the container already exists. But
-  // when I try to navigate back from profile page, I find tooltip won't
-  // show though the related element exists. I think there might be something
-  // else in javascript context, which is broken after navigation between
-  // pages. So instead of doing nothing, I remove the container and reload
-  // it again. At least this way works.
-  if (exists(`#${featureId}`)) {
-    await elementReady(`#${featureId}`); // should be waited or it cannot be removed
-    $(`#${featureId}`).remove();
-  }
-
   $('a[data-ga-click*="show fork modal"]').attr({
     'data-tip': '',
     'data-for': 'fork-tooltip',
@@ -49,19 +43,10 @@ const attach = async () => {
     'data-text-color': githubTheme === 'light' ? '#24292F' : '#C9D1D9',
     'data-background-color': githubTheme === 'light' ? 'white' : '#161B22',
   });
-  const tooltipContainer = document.createElement('div');
-  tooltipContainer.id = featureId;
-
-  render(<View forks={forks} />, tooltipContainer);
-
-  await elementReady('#repository-container-header');
-  $('#repository-container-header').append(tooltipContainer);
-};
-
-const init = async (): Promise<void> => {
-  repoName = getRepoName();
-  await getData();
-  await attach();
+  const container = document.createElement('div');
+  container.id = featureId;
+  renderTo(container);
+  (await elementReady('#repository-container-header'))?.append(container);
 };
 
 const restore = async () => {
@@ -71,7 +56,10 @@ const restore = async () => {
     repoName = getRepoName();
     await getData();
   }
-  await attach();
+  // Ideally, we should do nothing if the container already exists. But after a tubor
+  // restoration visit, tooltip cannot be triggered though it exists in DOM tree. One
+  // way to solve this is to rerender the view to the container. At least this way works.
+  renderTo($(`#${featureId}`)[0]);
 };
 
 features.add(featureId, {
