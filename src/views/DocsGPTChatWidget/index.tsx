52960da3f377e655ad82618d1e5acbcc88d21e01
Lam Tang <tangyenan@gmail.com>
2023-03-14 12:35:33 +0800

    add prompts for requesting for new docs

--- src/views/DocsGPTChatWidget/index.tsx
+++ src/views/DocsGPTChatWidget/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Widget,
   addResponseMessage,
@@ -32,7 +32,20 @@ const handleNewUserMessage = async (newMessage: string) => {
 const View = ({ currentRepo, currentDocsName }: Props): JSX.Element => {
   const subtitle = currentDocsName
     ? `Ask anything about ${currentRepo}`
-    : `Currently not available for ${currentRepo}`;
+    : `NOT AVAILABLE for ${currentRepo}`;
+
+  useEffect(() => {
+    if (currentDocsName) {
+      addResponseMessage(
+        `Hi, I'm an assistant powered by [DocsGPT](https://github.com/arc53/docsgpt), a GPT-3 powered chatbot that answers questions about documentation. Ask me anything about \`${currentRepo}\`!`
+      );
+    } else {
+      addResponseMessage(
+        `OSS-GPT currently is **NOT AVAILABLE** for \`${currentRepo}\`, if you want docs support for the repository, please visit [this](https://github.com/hypertrons/hypertrons-crx/issues) issue and make a request there :)\n\nSee [all available docs](https://oss.x-lab.info/hypercrx/docsgpt_active_docs.json)`
+      );
+    }
+  }, [currentDocsName]);
+
   return (
     <Widget
       title="OSS-GPT"
