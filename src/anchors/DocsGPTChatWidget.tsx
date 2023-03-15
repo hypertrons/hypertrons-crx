55c045e439eba4fcf98dd16447b56bb4b5e5e23a
Lam Tang <tangyenan@gmail.com>
2023-03-15 13:55:14 +0800

    workaround to change emoji picker color

--- src/anchors/DocsGPTChatWidget.tsx
+++ src/anchors/DocsGPTChatWidget.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
+import { render } from 'react-dom';
 import $ from 'jquery';
 import { utils } from 'github-url-detection';
 
