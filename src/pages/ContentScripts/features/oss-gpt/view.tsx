5e4fa186c7c45b924c41aa81f46a1d70ce232dda
KUNAL <goudanikunal3@gmail.com>
2023-04-28 08:59:10 +0530

    fix:removes event listeners more accurately

--- src/pages/ContentScripts/features/oss-gpt/view.tsx
+++ src/pages/ContentScripts/features/oss-gpt/view.tsx
@@ -35,8 +35,6 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
   const [inited, setInited] = useState(false);
   const [settings, setSettings] = useState(new Settings());
   const [history, setHistory] = useState<[string, string]>(['', '']);
-  // Toggle to allow resizing of the widget
-  const [resizable, setResizable] = useState(true);
 
   useEffect(() => {
     const initSettings = async () => {
@@ -94,46 +92,51 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     // Options for the observer (which mutations to observe)
     const config = { attributes: true, childList: true, subtree: true };
     // Callback function to execute when mutations are observed
+    function exists(selector: JQuery<HTMLElement>) {
+      return $(selector).length > 0;
+    }
     const callback: MutationCallback = (mutationList, observer) => {
-      if (
-        $('div.rcw-conversation-resizer').length == 0 &&
-        $('div.rcw-conversation-container').length != 0
-      ) {
+      const conversationContainer = $('.rcw-conversation-container');
+      const conversationResizer = $('.rcw-conversation-resizer');
+      if (!exists(conversationResizer) && exists(conversationContainer)) {
         //we only add a resizer when there is no resizer and there is an active  conversation container
-        $('div.rcw-conversation-container').prepend(
-          "<div class='rcw-conversation-resizer'></div>"
-        );
+        const newContainer = $('<div>');
+        newContainer.attr('class', 'rcw-conversation-resizer');
+        conversationContainer.prepend(newContainer);
       } else {
-        if (resizable) {
-          // if resizer exits we only add listeners to it when resizable is true
-          const conversationContainer = $('.rcw-conversation-container');
-          const conversationResizer = $('.rcw-conversation-resizer');
-          let isDragging = false;
-          let initialMouseX: number;
-          let initialWidth: number;
-          if (conversationResizer.length) {
-            conversationResizer.on('mousedown', (event) => {
-              isDragging = true;
-              initialMouseX = event.clientX;
-              initialWidth = parseInt(conversationContainer.css('width'), 10);
-            });
-            $(document).on('mousemove', (event) => {
-              if (isDragging) {
-                const mouseX = event.clientX;
-                const widthDiff = mouseX - initialMouseX;
-                conversationContainer.css(
-                  'width',
-                  `${initialWidth - widthDiff}px`
-                );
-              }
-            });
-            $(document).on('mouseup', () => {
-              isDragging = false;
-            });
-          }
+        // if resizer exits we only add listeners to it when resizable is true
+        let isDragging = false;
+        let initialMouseX: number;
+        let initialWidth: number;
+        if (exists(conversationResizer)) {
+          let handleMouseDown: (event: JQuery.MouseDownEvent) => void;
+          let handleMouseMove: (event: JQuery.MouseMoveEvent) => void;
+          let handleMouseUp: (event: JQuery.MouseUpEvent) => void;
+          handleMouseDown = (event: JQuery.MouseDownEvent) => {
+            isDragging = true;
+            initialMouseX = event.clientX;
+            initialWidth = parseInt(conversationContainer.css('width'), 10);
+          };
+          handleMouseMove = (event: JQuery.MouseMoveEvent) => {
+            if (isDragging) {
+              console.log(document);
+              const mouseX = event.clientX;
+              const widthDiff = mouseX - initialMouseX;
+              conversationContainer.css(
+                'width',
+                `${initialWidth - widthDiff}px`
+              );
+            }
+          };
+          handleMouseUp = (event: JQuery.MouseUpEvent) => {
+            isDragging = false;
+          };
+          conversationResizer.on('mousedown', handleMouseDown);
+          $(document).on('mousemove', handleMouseMove);
+          $(document).on('mouseup', handleMouseUp);
         }
       }
-      if ($('section.emoji-mart').length > 0) {
+      if (exists($('section.emoji-mart'))) {
         $('section.emoji-mart').addClass(`emoji-mart emoji-mart-${theme}`);
       }
     };
@@ -143,12 +146,16 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     observer.observe(targetNode, config);
 
     return () => {
-      // Later, you can stop observing
+      // Stop observing
       observer.disconnect();
-
-      if ($('.rcw-conversation-resizer')) {
-        $('.rcw-conversation-resizer').off('mousedown');
-        $(document).off('mousemove mouseup');
+      const conversationResizer = $('.rcw-conversation-resizer');
+      if (exists(conversationResizer)) {
+        const handleMouseDown = conversationResizer.data('handleMouseDown');
+        const handleMouseMove = conversationResizer.data('handleMouseMove');
+        const handleMouseUp = conversationResizer.data('handleMouseUp');
+        conversationResizer.off('mousedown', handleMouseDown);
+        $(document).off('mousemove', handleMouseMove);
+        $(document).off('mouseup', handleMouseUp);
       }
     };
   }, []);
