df465d188818236c0f24ab546ba43abbd3c5641e
KUNAL <goudanikunal3@gmail.com>
2023-04-29 12:17:38 +0530

    fix:resize implementation of gpt widget

--- src/pages/ContentScripts/features/oss-gpt/view.tsx
+++ src/pages/ContentScripts/features/oss-gpt/view.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import $ from 'jquery';
 import {
   Widget,
@@ -10,6 +10,7 @@ import {
 
 import { getAnswer } from './service';
 import './rcw.scss';
+import exists from '../../../../helpers/exists';
 import { getMessageByLocale } from '../../../../utils/utils';
 import Settings, { loadSettings } from '../../../../utils/settings';
 
@@ -35,6 +36,8 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
   const [inited, setInited] = useState(false);
   const [settings, setSettings] = useState(new Settings());
   const [history, setHistory] = useState<[string, string]>(['', '']);
+  const mouseDownX = useRef(0); // X position when mouse down
+  const rcwWidth = useRef(0); // rcw width when mouse down
 
   useEffect(() => {
     const initSettings = async () => {
@@ -85,6 +88,28 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     }
   }, [settings, currentRepo, currentDocsName]);
 
+  const handleMouseDown = (event: JQuery.MouseDownEvent) => {
+    mouseDownX.current = event.clientX;
+    rcwWidth.current = parseInt(
+      $('#rcw-conversation-container').css('width'),
+      10
+    );
+    $(document).on('mousemove', handleMouseMove);
+    $(document).on('mouseup', handleMouseUp);
+  };
+
+  const handleMouseMove = (event: JQuery.MouseMoveEvent) => {
+    $('#rcw-conversation-container').css(
+      'width',
+      `${rcwWidth.current - (event.clientX - mouseDownX.current)}px`
+    );
+  };
+
+  const handleMouseUp = (event: JQuery.MouseUpEvent) => {
+    $(document).off('mousemove', handleMouseMove);
+    $(document).off('mouseup', handleMouseUp);
+  };
+
   // we cannot change emoji-mart theme with an option, so we have to use MutationObserver and jquery to change the css
   useEffect(() => {
     // Select the node that will be observed for mutations
@@ -92,51 +117,20 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     // Options for the observer (which mutations to observe)
     const config = { attributes: true, childList: true, subtree: true };
     // Callback function to execute when mutations are observed
-    function exists(selector: JQuery<HTMLElement>) {
-      return $(selector).length > 0;
-    }
     const callback: MutationCallback = (mutationList, observer) => {
-      const conversationContainer = $('.rcw-conversation-container');
-      const conversationResizer = $('.rcw-conversation-resizer');
-      if (!exists(conversationResizer) && exists(conversationContainer)) {
-        //we only add a resizer when there is no resizer and there is an active  conversation container
-        const newContainer = $('<div>');
-        newContainer.attr('class', 'rcw-conversation-resizer');
-        conversationContainer.prepend(newContainer);
-      } else {
-        // if resizer exits we only add listeners to it when resizable is true
-        let isDragging = false;
-        let initialMouseX: number;
-        let initialWidth: number;
-        if (exists(conversationResizer)) {
-          let handleMouseDown: (event: JQuery.MouseDownEvent) => void;
-          let handleMouseMove: (event: JQuery.MouseMoveEvent) => void;
-          let handleMouseUp: (event: JQuery.MouseUpEvent) => void;
-          handleMouseDown = (event: JQuery.MouseDownEvent) => {
-            isDragging = true;
-            initialMouseX = event.clientX;
-            initialWidth = parseInt(conversationContainer.css('width'), 10);
-          };
-          handleMouseMove = (event: JQuery.MouseMoveEvent) => {
-            if (isDragging) {
-              console.log(document);
-              const mouseX = event.clientX;
-              const widthDiff = mouseX - initialMouseX;
-              conversationContainer.css(
-                'width',
-                `${initialWidth - widthDiff}px`
-              );
-            }
-          };
-          handleMouseUp = (event: JQuery.MouseUpEvent) => {
-            isDragging = false;
-          };
-          conversationResizer.on('mousedown', handleMouseDown);
-          $(document).on('mousemove', handleMouseMove);
-          $(document).on('mouseup', handleMouseUp);
-        }
+      // hacking code for resizer
+      if (
+        !exists('.rcw-conversation-resizer') &&
+        exists('.rcw-conversation-container')
+      ) {
+        //we only add a resizer when conversation container is active
+        const resizerDiv = $('<div>');
+        resizerDiv.attr('class', 'rcw-conversation-resizer');
+        resizerDiv.on('mousedown', handleMouseDown);
+        $('#rcw-conversation-container').prepend(resizerDiv);
       }
-      if (exists($('section.emoji-mart'))) {
+      // hacking code for emoji-mart
+      if (exists('section.emoji-mart')) {
         $('section.emoji-mart').addClass(`emoji-mart emoji-mart-${theme}`);
       }
     };
@@ -148,15 +142,6 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     return () => {
       // Stop observing
       observer.disconnect();
-      const conversationResizer = $('.rcw-conversation-resizer');
-      if (exists(conversationResizer)) {
-        const handleMouseDown = conversationResizer.data('handleMouseDown');
-        const handleMouseMove = conversationResizer.data('handleMouseMove');
-        const handleMouseUp = conversationResizer.data('handleMouseUp');
-        conversationResizer.off('mousedown', handleMouseDown);
-        $(document).off('mousemove', handleMouseMove);
-        $(document).off('mouseup', handleMouseUp);
-      }
     };
   }, []);
 
