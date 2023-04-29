9bfbddc8be32dfb691c05b679abf91ba7324bb1f
Kunal Patel <goudanikunal3@gmail.com>
2023-04-29 12:23:11 +0530

    fix: abnormal-width on text select (#658)
    * fix:Abnormal-width on text select
    
    * fix:removes event listeners more accurately
    
    * fix:resize implementation of gpt widget

--- src/pages/ContentScripts/features/oss-gpt/view.tsx
+++ src/pages/ContentScripts/features/oss-gpt/view.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import $ from 'jquery';
 import {
   Widget,
@@ -10,6 +10,7 @@ import {
 
 import { getAnswer } from './service';
 import './rcw.scss';
+import exists from '../../../../helpers/exists';
 import { getMessageByLocale } from '../../../../utils/utils';
 import Settings, { loadSettings } from '../../../../utils/settings';
 
@@ -35,6 +36,8 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
   const [inited, setInited] = useState(false);
   const [settings, setSettings] = useState(new Settings());
   const [history, setHistory] = useState<[string, string]>(['', '']);
+  const mouseDownX = useRef(0); // X position when mouse down
+  const rcwWidth = useRef(0); // rcw width when mouse down
 
   useEffect(() => {
     const initSettings = async () => {
@@ -85,6 +88,28 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     }
   }, [settings, currentRepo, currentDocsName]);
 
+  const handleMouseDown = (event: JQuery.MouseDownEvent) => {
+    mouseDownX.current = event.clientX;
+    rcwWidth.current = parseInt(
+      $('#rcw-conversation-container').css('width'),
+      10
+    );
+    $(document).on('mousemove', handleMouseMove);
+    $(document).on('mouseup', handleMouseUp);
+  };
+
+  const handleMouseMove = (event: JQuery.MouseMoveEvent) => {
+    $('#rcw-conversation-container').css(
+      'width',
+      `${rcwWidth.current - (event.clientX - mouseDownX.current)}px`
+    );
+  };
+
+  const handleMouseUp = (event: JQuery.MouseUpEvent) => {
+    $(document).off('mousemove', handleMouseMove);
+    $(document).off('mouseup', handleMouseUp);
+  };
+
   // we cannot change emoji-mart theme with an option, so we have to use MutationObserver and jquery to change the css
   useEffect(() => {
     // Select the node that will be observed for mutations
@@ -93,7 +118,19 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     const config = { attributes: true, childList: true, subtree: true };
     // Callback function to execute when mutations are observed
     const callback: MutationCallback = (mutationList, observer) => {
-      if ($('section.emoji-mart').length > 0) {
+      // hacking code for resizer
+      if (
+        !exists('.rcw-conversation-resizer') &&
+        exists('.rcw-conversation-container')
+      ) {
+        //we only add a resizer when conversation container is active
+        const resizerDiv = $('<div>');
+        resizerDiv.attr('class', 'rcw-conversation-resizer');
+        resizerDiv.on('mousedown', handleMouseDown);
+        $('#rcw-conversation-container').prepend(resizerDiv);
+      }
+      // hacking code for emoji-mart
+      if (exists('section.emoji-mart')) {
         $('section.emoji-mart').addClass(`emoji-mart emoji-mart-${theme}`);
       }
     };
@@ -103,7 +140,7 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
     observer.observe(targetNode, config);
 
     return () => {
-      // Later, you can stop observing
+      // Stop observing
       observer.disconnect();
     };
   }, []);
@@ -114,7 +151,6 @@ const View = ({ theme, currentRepo, currentDocsName }: Props): JSX.Element => {
         title="OSS-GPT"
         subtitle={subtitle}
         emojis={true} // will be enabled after style is fine tuned for two themes
-        resizable={true}
         handleNewUserMessage={handleNewUserMessage}
         showBadge={false}
         profileAvatar={chrome.runtime.getURL('main.png')}
