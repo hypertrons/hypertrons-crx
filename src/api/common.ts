eea6a14c22d8a2e49ae62b59b59bdcc5dd3ecacd
Lam Tang <tangyenan@gmail.com>
2023-08-13 15:24:53 +0800

    feat: a `hasMeta` function with cache support to check if the meta file exists

--- src/api/common.ts
+++ src/api/common.ts
@@ -22,3 +22,25 @@ export const getMetricByName = async (
     }
   }
 };
+
+const metaCache = new Map<string, Promise<Response>>();
+/**
+ * Check if the repo/user has meta data in OpenDigger
+ * e.g. https://oss.x-lab.info/open_digger/github/X-lab2017/open-digger/meta.json (repo meta file)
+ * e.g. https://oss.x-lab.info/open_digger/github/tyn1998/meta.json (user meta file)
+ * @param name repo name or user name
+ */
+export const hasMeta = async (name: string) => {
+  let promise: Promise<Response>;
+  if (!metaCache.has(name)) {
+    const url = `${OSS_XLAB_ENDPOINT}/open_digger/github/${name}/meta.json`;
+    promise = fetch(url);
+    metaCache.set(name, promise);
+  }
+  const response = await metaCache.get(name)!;
+  if (!response.ok) {
+    return false;
+  } else {
+    return true;
+  }
+};
