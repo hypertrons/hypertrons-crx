2e5abf48c3be68dffb942253a0ac8581e66a8960
andyhuang18 <50283262+andyhuang18@users.noreply.github.com>
2023-08-14 00:15:01 +0800

    feat: add Repo Activity Racing Bar (#696)
    * feat: add activity racing bar
    
    feat: initial implementation of activity racing bar(need to be modified)
    
    feat: initial implementation of activity racing bar(need to be modified v1)
    
    feat: initial implementation of activity racing bar(need to be modified v2)
    
    feat: implementation of activity racing bar
    
    feat: change the style of chart and button
    
    * feat: change chart width and put button to corner
    
    * feat: support for i18n
    
    * feat: update .gitignore for .idea files
    
    * style: better style makes it look better
    
    * chore: let and const has their own scope
    
    * chore: better way to play and replay the racing bar
    
    * chore: update messages
    
    * feat: variable bar number according to contributors number
    
    * fix: fix performance issue
    
    ---------
    
    Co-authored-by: Lam Tang <tangyenan@gmail.com>

---
+++ src/pages/ContentScripts/features/repo-activity-racing-bar/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render, Container } from 'react-dom';
+import $ from 'jquery';
+
+import features from '../../../../feature-manager';
+import isPerceptor from '../../../../helpers/is-perceptor';
+import { getRepoName } from '../../../../helpers/get-repo-info';
+import { getActivityDetails } from '../../../../api/repo';
+import View from './view';
+import DataNotFound from '../repo-networks/DataNotFound';
+import * as pageDetect from 'github-url-detection';
+
+const featureId = features.getFeatureID(import.meta.url);
+let repoName: string;
+let repoActivityDetails: any;
+
+const getData = async () => {
+  repoActivityDetails = await getActivityDetails(repoName);
+};
+
+const renderTo = (container: Container) => {
+  if (!repoActivityDetails) {
+    render(<DataNotFound />, container);
+    return;
+  }
+  render(
+    <View currentRepo={repoName} repoActivityDetails={repoActivityDetails} />,
+    container
+  );
+};
+
+const init = async (): Promise<void> => {
+  repoName = getRepoName();
+  await getData();
+  const container = document.createElement('div');
+  container.id = featureId;
+  renderTo(container);
+  const parentElement = document.getElementById('hypercrx-perceptor-layout');
+  if (parentElement) {
+    parentElement.append(container);
+  }
+};
+
+const restore = async () => {
+  // Clicking another repo link in one repo will trigger a turbo:visit,
+  // so in a restoration visit we should be careful of the current repo.
+  if (repoName !== getRepoName()) {
+    repoName = getRepoName();
+  }
+  // rerender the chart or it will be empty
+  renderTo($(`#${featureId}`)[0]);
+};
+
+features.add(featureId, {
+  asLongAs: [isPerceptor],
+  awaitDomReady: false,
+  init,
+  restore,
+});
---
+++ src/pages/ContentScripts/features/repo-activity-racing-bar/view.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+
+import getMessageByLocale from '../../../../helpers/get-message-by-locale';
+import optionsStorage, {
+  HypercrxOptions,
+  defaults,
+} from '../../../../options-storage';
+import RacingBar from './RacingBar';
+
+interface Props {
+  currentRepo: string;
+  repoActivityDetails: any;
+}
+
+const View = ({ currentRepo, repoActivityDetails }: Props): JSX.Element => {
+  const [options, setOptions] = useState<HypercrxOptions>(defaults);
+  const [replay, setReplay] = useState(0);
+
+  useEffect(() => {
+    (async function () {
+      setOptions(await optionsStorage.getAll());
+    })();
+  }, []);
+
+  const handleReplayClick = () => {
+    setReplay(replay + 1);
+  };
+
+  return (
+    <div>
+      <div className="hypertrons-crx-border hypertrons-crx-container">
+        <div className="hypertrons-crx-title">
+          <span>
+            {getMessageByLocale(
+              'component_projectRacingBar_title',
+              options.locale
+            )}
+          </span>
+          <div className="hypertrons-crx-title-extra developer-tab">
+            <button className="replay-button" onClick={handleReplayClick}>
+              {getMessageByLocale(
+                'component_projectRacingBar_ReplayButton',
+                options.locale
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="d-flex flex-wrap flex-items-center">
+          <div className="col-12 col-md-8">
+            <div style={{ margin: '10px 0 20px 20px' }}>
+              <RacingBar
+                key={replay}
+                repoName={currentRepo}
+                data={repoActivityDetails}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-4">
+            <div
+              className="color-text-secondary"
+              style={{ marginLeft: '35px', marginRight: '35px' }}
+            >
+              <p>
+                {getMessageByLocale(
+                  'component_projectRacingBar_description',
+                  options.locale
+                )}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default View;
