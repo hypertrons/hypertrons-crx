adfa7d973614332c1cfc50c8382ad36241d1bc19
wj23027 <809711241@qq.com>
2023-10-15 22:48:06 +0800

    feat: Modify the 'get newest month' function

--- src/helpers/get-newest-month.ts
+++ src/helpers/get-newest-month.ts
@@ -1,10 +1,13 @@
 const getNewestMonth = () => {
-  const lastDataAvailableMonth = new Date();
-  lastDataAvailableMonth.setDate(0);
+  const now = new Date();
+  if (now.getDate() === 1) {
+    // data for last month is not ready in the first day of the month (#595)
+    now.setDate(0); // a way to let month - 1
+  }
+  now.setDate(0); // see issue #632
+
   return (
-    lastDataAvailableMonth.getFullYear() +
-    '-' +
-    (lastDataAvailableMonth.getMonth() + 1).toString().padStart(2, '0')
+    now.getFullYear() + '-' + (now.getMonth() + 1).toString().padStart(2, '0')
   );
 };
 
