0b90d6d807db7c308426ee39829b1cab5569b787
AndyHuang <763230412@qq.com>
2023-10-15 13:30:52 +0800

    refactor: implement the collection button in the original GitHub style (step 1)

--- src/pages/ContentScripts/features/repo-collection/index.tsx
+++ src/pages/ContentScripts/features/repo-collection/index.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import { render, Container } from 'react-dom';
+import { render } from 'react-dom';
 import $ from 'jquery';
 import elementReady from 'element-ready';
-import * as pageDetect from 'github-url-detection';
 
 import features from '../../../../feature-manager';
-import { getDeveloperName } from '../../../../helpers/get-developer-info';
 import isPublicRepo from '../../../../helpers/is-public-repo';
 import View from './view';
-import getGithubTheme from '../../../../helpers/get-github-theme';
-import MyDropdown from './MyDropdown';
+import { CollectionButton } from './CollectionButton';
 
 const featureId = features.getFeatureID(import.meta.url);
 
 const init = async (): Promise<void> => {
-  const myDropdownContainer = document.createElement('div');
-  myDropdownContainer.id = featureId;
-  render(<MyDropdown />, myDropdownContainer);
-  await elementReady('#repository-container-header');
-  $('#repository-details-container').before(myDropdownContainer);
-
-  const viewContainer = document.createElement('div');
-  viewContainer.id = featureId;
-  render(<View />, viewContainer);
-  $('body').append(viewContainer);
+  // insert the button
+  const buttonContainer = document.createElement('li');
+  buttonContainer.id = `${featureId}-button`;
+  render(<CollectionButton />, buttonContainer);
+  await elementReady('#repository-details-container');
+  $('#repository-details-container>ul').prepend(buttonContainer);
+  // insert the modal
+  const modalContainer = document.createElement('div');
+  modalContainer.id = `${featureId}-modal`;
+  render(<View />, modalContainer);
+  $('body').append(modalContainer);
 };
 
 features.add(featureId, {
