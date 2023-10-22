b408207b0a1e3fa00f8b1034cb4d276365836bfb
AndyHuang <763230412@qq.com>
2023-10-22 12:18:10 +0800

    chore: fix typo and add validator for duplicate collection name

--- src/pages/ContentScripts/features/repo-collection/CollectionModal/CollectionEditor.tsx
+++ src/pages/ContentScripts/features/repo-collection/CollectionModal/CollectionEditor.tsx
@@ -44,60 +44,16 @@ interface DataSourceType {
   name: string;
   description: string;
 }
-/*
-async function getUserOrOrgRepos(
-  username: string,
-  isOrg: boolean,
-  accessToken: string // GitHub Personal Access Token
-): Promise<RepositoryInfo[]> {
-  try {
-    const apiUrl = isOrg
-      ? `https://api.github.com/orgs/${username}/repos`
-      : `https://api.github.com/users/${username}/repos`;
 
-    const response = await fetch(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+const accessTokens = ['token'];
 
-    if (!response.ok) {
-      throw new Error(
-        `GitHub API request failed with status: ${response.status}`
-      );
-    }
-
-    const reposData = await response.json();
-
-    const repositories: RepositoryInfo[] = reposData.map((repo: any) => ({
-      name: repo.name,
-      description: repo.description || '',
-    }));
-
-    return repositories;
-  } catch (error) {
-    console.error('Error fetching repositories:', error);
-    throw error;
-  }
-}
-*/
-// 定义多个 GitHub Personal Access Token
-const accessTokens = [
-  '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',
-  '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',
-  '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',
-  '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',
-  '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',
-]; // 添加你的令牌
-
-let currentTokenIndex = 0; // 当前使用的令牌索引
+let currentTokenIndex = 0;
 
 async function getUserOrOrgRepos(
   username: string,
   isOrg: boolean
 ): Promise<RepositoryInfo[]> {
   try {
-    // 获取当前使用的令牌
     const currentAccessToken = accessTokens[currentTokenIndex];
 
     const apiUrl = isOrg
@@ -111,12 +67,10 @@ async function getUserOrOrgRepos(
     });
 
     if (!response.ok) {
-      // 如果收到401错误，尝试下一个令牌
       if (response.status === 401) {
-        currentTokenIndex = (currentTokenIndex + 1) % accessTokens.length; // 切换到下一个令牌
-        return getUserOrOrgRepos(username, isOrg); // 递归调用以使用下一个令牌
+        currentTokenIndex = (currentTokenIndex + 1) % accessTokens.length; // switch to next token
+        return getUserOrOrgRepos(username, isOrg);
       } else {
-        // 其他错误，抛出错误
         throw new Error(
           `GitHub API request failed with status: ${response.status}`
         );
@@ -158,6 +112,7 @@ const CollectionEditor: React.FC<CollectionEditorProps> = ({
   collectionName,
   collectionData,
 }) => {
+  const { allCollections } = useRepoCollectionContext();
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
   const [form] = Form.useForm();
   const [dataSource, setDataSource] = useState<DataSourceType[]>();
@@ -177,7 +132,7 @@ const CollectionEditor: React.FC<CollectionEditorProps> = ({
     return {
       key: collectionData.indexOf(repositoryName).toString(),
       name: repositoryName,
-      description: repoData.description || '', // 如果没有描述信息，则设为空字符串
+      description: repoData.description || '',
     };
   }
 
@@ -196,16 +151,14 @@ const CollectionEditor: React.FC<CollectionEditorProps> = ({
   }, []);
 
   const initialValues = {
-    collectionName: isEdit ? collectionName : '', // 设置字段的初始值
+    collectionName: isEdit ? collectionName : '',
   };
-  const modalTitle = isEdit ? 'Collection Editor' : 'Creat a new collection';
+  const modalTitle = isEdit ? 'Collection Editor' : 'Create a new collection';
 
   const onSelectChange = (
     newSelectedRowKeys: React.Key[],
     selectedRows: DataType[]
   ) => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
-    console.log('selected Rows', selectedRows);
     setNewRepoData(selectedRows.map((item) => item.name));
     setSelectedRowKeys(newSelectedRowKeys);
   };
@@ -220,13 +173,10 @@ const CollectionEditor: React.FC<CollectionEditorProps> = ({
   };
 
   const handleInquireClick = () => {
-    // 在点击 "inquire" 按钮时，获取 Quick import 输入框的值
     const inputValue = form.getFieldValue('Quick import');
     async function fetchRepositories() {
       try {
         const result = await getUserOrOrgRepos(inputValue, isOrg);
-        // 在这里可以访问 "repositories"，它将包含获取的仓库信息
-        console.log('Repositories:', result);
         let nextKey: number;
         if (dataSource) {
           nextKey = dataSource.length + 1;
@@ -290,12 +240,22 @@ const CollectionEditor: React.FC<CollectionEditorProps> = ({
           rules={[
             {
               required: true,
-              message: 'Please input the title of collection!',
+              message: 'Please input the name of collection!',
+            },
+            {
+              validator: (rule, value) => {
+                const existingNames = allCollections;
+                if (existingNames.some((item) => item.name === value)) {
+                  return Promise.reject('Collection name already exists.');
+                }
+                return Promise.resolve();
+              },
             },
           ]}
         >
-          <Input placeholder={'input name here'} />
+          <Input placeholder="Input name here" />
         </Form.Item>
+
         <Form.Item name="Quick import" label="Quick import">
           <Input
             type="textarea"
