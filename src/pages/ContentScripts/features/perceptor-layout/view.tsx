837bfa00da1ee504edd973cba5ba1cf7f8854f52
Harry <xiaohanwu12@gmail.com>
2023-10-22 11:33:57 -0400

    Fix: inconsistent graphs layout

--- src/pages/ContentScripts/features/perceptor-layout/view.tsx
+++ src/pages/ContentScripts/features/perceptor-layout/view.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const View = (): JSX.Element => {
-  return <></>;
+  return (
+    <>
+      <div id="hypercrx-repo-activity-racing-bar" />
+      <div id="hypercrx-repo-networks" />
+    </>
+  );
 };
 
 export default View;
--- src/pages/ContentScripts/features/repo-networks/index.tsx
+++ src/pages/ContentScripts/features/repo-networks/index.tsx
@@ -43,7 +43,7 @@ const init = async (): Promise<void> => {
   const container = document.createElement('div');
   container.id = featureId;
   renderTo(container);
-  $('#hypercrx-perceptor-layout').append(container);
+  $('#hypercrx-repo-networks').append(container);
 };
 
 const restore = async () => {
