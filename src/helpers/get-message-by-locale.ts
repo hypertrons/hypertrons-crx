c190147db6e4ce28b5320291e927b3cf2ea4cd13
taketaketakeru <taketaketakeru@163.com>
2024-06-20 17:07:46 +0800

    comment the code in get-message-by-locale.ts

--- src/helpers/get-message-by-locale.ts
+++ src/helpers/get-message-by-locale.ts
@@ -1,12 +1,12 @@
-import messages_en from '../locales/en/messages.json';
-import messages_zh_CN from '../locales/zh_CN/messages.json';
+// import messages_en from '../locales/en/messages.json';
+// import messages_zh_CN from '../locales/zh_CN/messages.json';
 
-const messages_locale = {
-  en: messages_en,
-  zh_CN: messages_zh_CN,
-};
+// const messages_locale = {
+//   en: messages_en,
+//   zh_CN: messages_zh_CN,
+// };
 
-export default function getMessageByLocale(key: string, locale: string) {
-  // @ts-ignore
-  return messages_locale[locale][key]['message'];
-}
+// export default function getMessageByLocale(key: string, locale: string) {
+//   // @ts-ignore
+//   return messages_locale[locale][key]['message'];
+// }
