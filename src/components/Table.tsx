5302d4e930757c5a36c22fc1e95fe9f93e9a205c
wouldmm <972663628@qq.com>
2024-07-28 02:27:14 +0800

    modify table style

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,6 +14,8 @@ const Table: React.FC<TableProps> = ({ adjacentNodes }) => {
   const cellStyle: React.CSSProperties = {
     textAlign: 'left',
     verticalAlign: 'middle',
+    whiteSpace: 'nowrap',
+    padding: '3px',
   };
 
   const containerStyle: React.CSSProperties = {
@@ -22,24 +24,38 @@ const Table: React.FC<TableProps> = ({ adjacentNodes }) => {
     margin: '10px 0 20px 20px',
   };
 
+  const column1Style: React.CSSProperties = {
+    ...cellStyle,
+    flex: '1 1 70%',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  };
+
+  const column2Style: React.CSSProperties = {
+    ...cellStyle,
+    flex: '0 0 30%',
+    textAlign: 'center',
+  };
+
   return (
     <div style={containerStyle}>
       <table style={tableStyle}>
         <thead>
-          <tr>
-            <th style={cellStyle}>Related Repository</th>
-            <th style={{ ...cellStyle, textAlign: 'center' }}>Relation value</th>
+          <tr style={{ display: 'flex' }}>
+            <th style={column1Style}>Related Repository</th>
+            <th style={column2Style}>Relation value</th>
           </tr>
         </thead>
         <tbody>
           {adjacentNodes.map((node, index) => (
-            <tr key={index}>
-              <td style={cellStyle}>
+            <tr key={index} style={{ display: 'flex' }}>
+              <td style={column1Style}>
                 <a
                   href={`https://github.com/${node[0]}`}
                   style={{ color: 'inherit' }}
                   target="_blank"
                   rel="noopener noreferrer"
+                  title={node[0]}
                 >
                   <img
                     src={`https://avatars.githubusercontent.com/${node[0].split('/')[0]}`}
@@ -49,7 +65,7 @@ const Table: React.FC<TableProps> = ({ adjacentNodes }) => {
                   {node[0]}
                 </a>
               </td>
-              <td style={{ ...cellStyle, textAlign: 'center' }}>{node[1]}</td>
+              <td style={column2Style}>{node[1]}</td>
             </tr>
           ))}
         </tbody>
