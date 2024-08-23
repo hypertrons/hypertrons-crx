cda73a10189cd2abd26005eb2bf5845481919d89
Tenth-crew <68376741+Tenth-crew@users.noreply.github.com>
2024-08-23 21:56:16 +0800

    fix: repo homepage latest commit hovercard openrank display (#849)
    * fix: repo homepage latest commit hovercard openrank display
    
    * Revert "fix: repo homepage latest commit hovercard openrank display"
    
    This reverts commit ed4c2fbd90720aed700d63cde006f8760a154e7b.
    
    * fix: repo homepage latest commit hovercard openrank display
    
    * Add continuous monitoring of elements
    
    * Prevent openrank from adding multiple times
    
    * Modify openrank data to the latest month
    
    * Limiting Jitter Operation

--- src/pages/ContentScripts/features/developer-hovercard-info/index.tsx
+++ src/pages/ContentScripts/features/developer-hovercard-info/index.tsx
@@ -6,14 +6,25 @@ import ReactDOM from 'react-dom';
 import View from './view';
 
 const featureId = features.getFeatureID(import.meta.url);
+let isInitialized = false;
 
 const getDeveloperLatestOpenrank = async (developerName: string): Promise<string | null> => {
   const data = await getOpenrank(developerName);
   if (data) {
-    const values = Object.values(data) as string[];
-    const latestValue = values[values.length - 1];
-    return latestValue;
+    // filter YYYY-MM
+    const monthKeys = Object.keys(data).filter((key) => /^\d{4}-\d{2}$/.test(key));
+
+    if (monthKeys.length === 0) {
+      return null;
+    }
+
+    monthKeys.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+    const latestMonthKey = monthKeys[monthKeys.length - 1];
+
+    return data[latestMonthKey];
   }
+
   return null;
 };
 
@@ -31,48 +42,94 @@ const renderTo = (container: HTMLElement, developerName: string, openrank: strin
   ReactDOM.render(<View developerName={developerName} openrank={openrank} />, openRankContainer);
 };
 
-const init = async (): Promise<void> => {
+const elementReadyWithTimeout = async (selector: string, options: { stopOnDomReady: boolean }, timeout: number) => {
+  return Promise.race([
+    elementReady(selector, options),
+    new Promise((_, reject) => setTimeout(() => reject(new Error(`Timeout waiting for ${selector}`)), timeout)),
+  ]);
+};
+
+const processElement = (element: Element) => {
+  const hovercardUrl = element.getAttribute('data-hovercard-url');
+  if (!hovercardUrl || !hovercardUrl.startsWith('/users')) {
+    return;
+  }
+
   let abortController = new AbortController();
-  const hovercardSelector = '[data-hovercard-type="user"]';
-  document.querySelectorAll(hovercardSelector).forEach((element) => {
-    // isProcessing is used to Prevent OpenRank from adding duplicates
-    element.addEventListener('mouseover', async () => {
-      abortController.abort();
-      abortController = new AbortController();
-      const signal = abortController.signal;
 
-      const developerName = getDeveloperName(element as HTMLElement) as string;
+  element.addEventListener('mouseover', async () => {
+    abortController.abort();
+    abortController = new AbortController();
+    const signal = abortController.signal;
+    await new Promise((resolve) => setTimeout(resolve, 600));
 
-      // Create a unique identifier for the popover
-      const popoverId = `popover-${developerName}`;
+    const developerName = getDeveloperName(element as HTMLElement) as string;
 
-      // Get the floating card container
-      const $popoverContainer =
-        'body > div.logged-in.env-production.page-responsive > div.Popover.js-hovercard-content.position-absolute > div > div > div';
-      const popover = await elementReady($popoverContainer, { stopOnDomReady: false });
+    // Create a unique identifier for the popover
+    const popoverId = `popover-${developerName}`;
 
-      const openRankDiv = popover?.querySelector('.hypercrx-openrank-info');
-      const existingDeveloperName = openRankDiv?.getAttribute('data-developer-name');
-      if (existingDeveloperName === developerName) {
-        return;
+    // Get the floating card container
+    const $popoverContainer =
+      'body > div.logged-in.env-production.page-responsive > div.Popover.js-hovercard-content.position-absolute > div > div > div';
+    const popover = await elementReady($popoverContainer, { stopOnDomReady: false });
+
+    const openRankDiv = popover?.querySelector('.hypercrx-openrank-info');
+    const existingDeveloperName = openRankDiv?.getAttribute('data-developer-name');
+    if (existingDeveloperName === developerName) {
+      return;
+    }
+    openRankDiv?.remove();
+
+    // Set the popover's unique identifier
+    // make the current OpenRank information and person match
+    popover?.setAttribute('data-popover-id', popoverId);
+
+    const openrank = await getDeveloperLatestOpenrank(developerName);
+
+    if (!openrank) {
+      return;
+    }
+
+    if (!signal.aborted && popover && popover.getAttribute('data-popover-id') === popoverId) {
+      // Check if the popover is still associated with the correct developer
+      renderTo(popover, developerName, openrank);
+    }
+  });
+};
+
+const init = async (): Promise<void> => {
+  if (isInitialized) return;
+  isInitialized = true;
+
+  const hovercardSelector = '[data-hovercard-url]';
+
+  await elementReady(hovercardSelector, { stopOnDomReady: false });
+  try {
+    await elementReadyWithTimeout('[data-testid=github-avatar]', { stopOnDomReady: false }, 1500);
+  } catch (error) {
+    console.log('The current interface does not have data-testid=github-avatar information');
+  }
+
+  // Initial processing of existing elements
+  document.querySelectorAll(hovercardSelector).forEach(processElement);
+
+  // Use MutationObserver to monitor dynamically added elements
+  const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach((node) => {
+          if (node instanceof HTMLElement) {
+            const newElements = node.querySelectorAll(hovercardSelector);
+            newElements.forEach(processElement);
+          }
+        });
       }
-      openRankDiv?.remove();
+    }
+  });
 
-      // Set the popover's unique identifier
-      // make the current OpenRank information and person match
-      popover?.setAttribute('data-popover-id', popoverId);
-
-      const openrank = await getDeveloperLatestOpenrank(developerName);
-
-      if (!openrank) {
-        return;
-      }
-
-      if (!signal.aborted && popover && popover.getAttribute('data-popover-id') === popoverId) {
-        // Check if the popover is still associated with the correct developer
-        renderTo(popover, developerName, openrank);
-      }
-    });
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
   });
 };
 
