dec09baef7756c6ed3bcf7a1cc4459fa8d2c0519
小喵喵不会喵喵叫 <75050154+Xsy41@users.noreply.github.com>
2024-11-09 11:55:46 +0800

    fix: Added a way to retrieve repository names through the page (#903)

--- src/helpers/get-developer-info.ts
+++ src/helpers/get-developer-info.ts
@@ -4,7 +4,19 @@ import $ from 'jquery';
 import * as pageDetect from 'github-url-detection';
 
 export function getDeveloperName() {
+  const developerNameByUrl = getDeveloperNameByUrl();
+  const developerNameByPage = getDeveloperNameByPage();
+  if (developerNameByUrl.toLowerCase() === developerNameByPage.toLowerCase()) {
+    return developerNameByPage;
+  }
+  return developerNameByUrl;
+}
+
+export function getDeveloperNameByPage() {
   return $('.p-nickname.vcard-username.d-block').text().trim().split(' ')[0];
+}
+export function getDeveloperNameByUrl() {
+  return pageDetect.utils.getUsername()!;
 }
 
 export async function isDeveloperWithMeta() {
