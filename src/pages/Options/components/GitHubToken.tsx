03734ad04ad1bc14f51a0af28825de0f881fc937
frank-zsy <syzhao1988@126.com>
2024-11-14 01:41:40 +0800

    fix: update auth scope
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/pages/Options/components/GitHubToken.tsx
+++ src/pages/Options/components/GitHubToken.tsx
@@ -35,7 +35,7 @@ const GitHubToken = () => {
     const clientId = 'Ov23liyofMsuQYwtfGLb';
     const redirectUri = 'http://8.147.129.123/github';
     const callback = chrome.identity.getRedirectURL();
-    const scope = encodeURIComponent('read:user');
+    const scope = encodeURIComponent('read:user, public_repo');
     const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token&state=${callback}`;
 
     chrome.identity.launchWebAuthFlow(
