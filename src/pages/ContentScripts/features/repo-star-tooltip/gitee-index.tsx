67fccb100853183c5973bf216a6e7be541591bdf
wangyantong2000 <18745996292@163.com>
2025-02-22 18:44:17 +0800

    first

---
+++ src/pages/ContentScripts/features/repo-star-tooltip/gitee-index.tsx
@@ -0,0 +1,54 @@
+import features from '../../../../feature-manager';
+import View from './view';
+import { checkLogined } from '../../../../helpers/get-github-developer-info';
+import elementReady from 'element-ready';
+import { getRepoName, hasRepoContainerHeader, isPublicRepoWithMeta } from '../../../../helpers/get-github-repo-info';
+import { getStars } from '../../../../api/repo';
+import { RepoMeta, metaStore } from '../../../../api/common';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import $ from 'jquery';
+import { getPlatform } from '../../../../helpers/get-platform';
+import isGitee from '../../../../helpers/is-gitee';
+import { GiteeNativePopover } from '../../components/GiteeNativePopover';
+const featureId = features.getFeatureID(import.meta.url);
+let repoName: string;
+let stars: any;
+let meta: RepoMeta;
+let platform: string;
+const getData = async () => {
+  stars = await getStars(platform, repoName);
+  meta = (await metaStore.get(platform, repoName)) as RepoMeta;
+};
+
+const init = async (): Promise<void> => {
+  platform = getPlatform();
+  repoName = getRepoName();
+  await getData();
+  const isLogined = checkLogined();
+  const starButtonSelector = isLogined ? 'button[data-ga-click*="star button"]' : 'a[data-hydro-click*="star button"]';
+  await elementReady(starButtonSelector);
+  // <div data-view-component="true" class="starred BtnGroup flex-1 ml-0">
+  // <div data-view-component="true" class="unstarred BtnGroup ml-0 flex-1">
+  // No matter the repo is starred or not, the two button are always there
+  // Select all star buttons and no more filtering
+
+  const $starButtons = $(starButtonSelector);
+  $starButtons.each(function () {
+    const placeholderElement = $('<div class="NativePopover" />').appendTo('body')[0];
+    createRoot(placeholderElement).render(
+      <GiteeNativePopover anchor={$(this)} width={280} arrowPosition="bottom">
+        <View stars={stars} meta={meta} />
+      </GiteeNativePopover>
+    );
+  });
+};
+
+const restore = async () => {};
+
+features.add(featureId, {
+  asLongAs: [isGitee, isPublicRepoWithMeta, hasRepoContainerHeader],
+  awaitDomReady: false,
+  init,
+  restore,
+});
